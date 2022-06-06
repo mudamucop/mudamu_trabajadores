88d938759ebe832fbc4a7a3b3a3a5e9cc52930e4
Aitor Oruna <a.oruna@mondragon-assembly.com>
2022-06-06 18:00:19 +0200

    adding customized error pages and changing ajax url

--- TRABAJADORES/src/main/java/com/Mudamu/MudamuApplication.java
+++ TRABAJADORES/src/main/java/com/Mudamu/MudamuApplication.java
@@ -1,7 +1,9 @@
 package com.Mudamu;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
--- TRABAJADORES/src/main/java/com/Mudamu/controller/CustomErrorController.java
+++ TRABAJADORES/src/main/java/com/Mudamu/controller/CustomErrorController.java
@@ -1,24 +1,43 @@
 
 package com.Mudamu.controller;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CustomErrorController implements ErrorController {
 
-    @GetMapping("/error")
-    public String getCustomError(@RequestHeader(name = "code") String errorCode) {
+    @GetMapping(value = "/error")
+    public String handleError(HttpServletRequest request) {
+        
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
-        return "404";
+        if (status != null) {
+        
+            Integer statusCode = Integer.valueOf(status.toString());
+
+            if(statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "errorPage/404";
+            }
+            else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return "errorPage/error";
+            }
+        }
+        return "error";
     }
 
     @Override
     public String getErrorPath() {
+        System.out.println("dsa");
         return "/error";
     }
 }
